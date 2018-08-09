93f814aee666465d1495840095b2a29a16d30621
Maxwell Giuliano <giulianomaxwell@outlook.com>
2018-08-09 12:23:56 -0400

    Added IndexHandler class to main.py

--- main.py
+++ main.py
@@ -6,3 +6,13 @@ the_jinja_env = jinja2.Environment(
 loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
 extensions=['jinja2.ext.autoescape'],
 autoescape=True)
+
+class IndexHandler(webapp2.RequestHandler):
+    def get(self):
+        index_template = the_ninja_env.get_template("Templates/index.html")
+        self.response.write(index_template.render())
+
+
+app = webapp2.WSGIApplication([
+    ('/', IndexHandler)
+], debug=True)
