0d7514c624ef28c7d72835410bc610543a0686ea
Shahan Mudasir <shahanmny@gmail.com>
2018-08-09 16:53:14 -0400

    worked on fortnite api in main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import webapp2
 import jinja2
 import os
+import json
+
 
 the_jinja_env = jinja2.Environment(
 loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -14,7 +16,13 @@ class IndexHandler(webapp2.RequestHandler):
 
 class StatsHandler(webapp2.RequestHandler):
     def get(self):
-        #fortnite_response = urlfetch.fetch("https://url").content
+        header = {'TRN-Api-Key': '44231534-d4ed-41fc-8e82-99ea6733085e'}
+        fortnite_response = urlfetch.fetch("https://api.fortnitetracker.com/v1/profile/" + platform + "/" + username, headers = headers).content
+        run = requests.get(fortnite_response, header)
+        platform = pc
+        username = Ninja
+        results = run.json()[lifeTimeStats]
+        print(results)
 
 app = webapp2.WSGIApplication([
     ('/', IndexHandler),
